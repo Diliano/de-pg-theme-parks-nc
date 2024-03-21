adc4e981d22829d52ce5a97272294f3c9288f746
DCrawley94 <danikacrawley.dev@gmail.com>
2024-03-21 09:37:06 +0000

    remove Makefile and requirements, update gitignore

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,1 @@
-# Write your tests for your utility functions here #
-
-def test_your_utility_functions_here():
-    '''Test your utility functions here'''
-    # Replace the assert with your test
-    assert 1 != 2
+# Write the tests for your utility functions here
