8579df82c2a06f147dcdd9862be7410d4bd1dd9b
nc-mick-fay <michael.fay@northcoders.com>
2024-08-15 15:11:34 +0100

    Refactor seed function to take database connection as argument

--- db/seed.py
+++ db/seed.py
@@ -3,16 +3,16 @@ from db.connection import db
 # before being able to run your seed file
 
 
-def seed(parks, rides, stalls, foods):
+def seed(db, parks, rides, stalls, foods):
     '''Seeds database'''
     db.run("DROP TABLE IF EXISTS rides;")
     db.run("DROP TABLE IF EXISTS stalls;")
     db.run("DROP TABLE IF EXISTS foods;")
     db.run("DROP TABLE IF EXISTS stalls_foods;")
     db.run("DROP TABLE IF EXISTS parks;")
-    create_parks()
+    create_parks(db)
 
 
-def create_parks():
+def create_parks(db):
     '''Create your parks table in the query below'''
     return db.run("")
