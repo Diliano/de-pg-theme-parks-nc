ab741b39633d7a662f7d294a94f7c0a8806a5c47
nc-mick-fay <michael.fay@northcoders.com>
2024-08-15 15:08:58 +0100

    Refactor run_seed to make use of connection functions

--- db/run_seed.py
+++ db/run_seed.py
@@ -1,11 +1,12 @@
 from db.seed import seed
-from db.connection import db
+from db.connection import create_conn, close_db
 from db.data.index import index as data
 
 # Do not change this code
 try:
-    seed(**data)
+    db = create_conn()
+    seed(db, **data)
 except Exception as e:
     print(e)
 finally:
-    db.close()
+    close_db(db)
