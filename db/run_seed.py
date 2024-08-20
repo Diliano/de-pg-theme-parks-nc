e013e87bc80f503e1f93ee4e8e39a088726e78ad
nc-mick-fay <michael.fay@northcoders.com>
2024-08-20 11:31:20 +0100

    Add line to avoid connection errors in run_seed file

--- db/run_seed.py
+++ db/run_seed.py
@@ -3,10 +3,12 @@ from db.connection import create_conn, close_db
 from db.data.index import index as data
 
 # Do not change this code
+db = None
 try:
     db = create_conn()
     seed(db, **data)
 except Exception as e:
     print(e)
 finally:
-    close_db(db)
+    if db:
+        close_db(db)
