1acfa3832b6662b4a791410e75fa025f65c89391
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-04 11:21:02 +0100

    create the create_conn() function and the close_db() function

--- db/connection.py
+++ db/connection.py
@@ -1,9 +1,19 @@
+from pg8000.native import Connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.environ["USER"]
+db = os.environ["DB"]
+
 # Create your create_conn function to return a database connection object    #
 
 def create_conn():
-    pass
+    return Connection(user=user, database=db)
+
 
 # Create a close_db function that closes a passed database connection object #
 
-def close_db():
-    pass
+def close_db(conn):
+    conn.close()
