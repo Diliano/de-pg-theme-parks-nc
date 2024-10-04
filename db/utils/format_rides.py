91c83958645c4797b5e6333cabafd3e04e6cbb18
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-04 16:04:50 +0100

    create and build functionality of the get_parks_data() utility function

--- db/utils/format_rides.py
+++ db/utils/format_rides.py
@@ -1,1 +1,18 @@
+from db.connection import create_conn, close_db
 # Create your utility functions here, feel free to make additional files
+
+
+def get_parks_data():
+    db = create_conn()
+    parks_data = db.run("""SELECT * FROM parks;""")
+    formatted_data = [
+        {
+            "park_id": park[0],
+            "park_name": park[1],
+            "year_opened": park[2],
+            "annual_attendance": park[3]
+        }
+        for park in parks_data
+    ]
+    close_db(db)
+    return formatted_data
