a648d4445cdcdaa7a446d815c95e3adcbf42c811
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-04 12:23:07 +0100

    create and build functionality for the create_rides() function

--- db/seed.py
+++ db/seed.py
@@ -10,6 +10,7 @@ def seed(db, parks, rides, stalls, foods):
     db.run("DROP TABLE IF EXISTS stalls_foods;")
     db.run("DROP TABLE IF EXISTS parks;")
     create_parks(db)
+    create_rides(db)
 
 
 def create_parks(db):
@@ -20,6 +21,19 @@ def create_parks(db):
             park_name VARCHAR(255) NOT NULL, 
             year_opened INTEGER NOT NULL, 
             annual_attendance INTEGER NOT NULL
+        );
+    """
+    return db.run(sql=create_query)
+
+
+def create_rides(db):
+    create_query = """
+        CREATE TABLE rides (
+            ride_id SERIAL PRIMARY KEY,
+            park_id INTEGER REFERENCES parks(park_id),
+            ride_name VARCHAR(255) NOT NULL,
+            year_opened INTEGER NOT NULL,
+            votes INTEGER NOT NULL
         );
     """
     return db.run(sql=create_query)
